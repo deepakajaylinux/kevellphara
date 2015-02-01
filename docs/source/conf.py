4a41606ae1f077abaab8e03691d1a72ec9fae81c
deepakajaylinux <amdeepak21@gmail.com>
2015-02-01 15:42:06 +0530

    pharaohtools

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -258,15 +258,15 @@ texinfo_documents = [
 
 language= 'en'
 locale_dirs = [
-		'locale/'
+		'../doc/source/locale/'
 	      ]  
 
-gettext_compact = True 
+gettext_compact = False 
 
+language= 'de'
+locale_dirs = [
+                'locale/'
+              ]
 
+gettext_compact = False
 
-#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-#if not on_rtd: # only import and set the theme if we're building docs locally
-#import sphinx_rtd_theme
-#html_theme = 'sphinx_rtd_theme'
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
